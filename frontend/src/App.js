13a6a4f88809e1fb8cafc0a57180603b3b648842
vepaan <141347566+vepaan@users.noreply.github.com>
2024-12-26 20:07:12 -0600

    frontend updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <button className='navbar-button'>Dashboard</button>
+        <button className='navbar-button'>Market</button>
+        <button className='navbar-button'>Companies</button>
+        <button className='navbar-button'>Info</button>
+      </div>
     </div>
   );
 }
