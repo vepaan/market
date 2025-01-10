aad9fb947ea2134f7f0a77132b80be0334ee8d25
vepaan <141347566+vepaan@users.noreply.github.com>
2025-01-09 20:37:38 -0600

    added order form

--- frontend/src/pages/placeorder.js
+++ frontend/src/pages/placeorder.js
@@ -4,7 +4,14 @@ import '../App.css';
 function PlaceOrder() {
   return (
     <div className="order-form">
-        Place Order
+        <form>
+            <label htmlFor="quantity">Quantity</label>
+            <input type="number" id="quantity" name="quantity" placeholder="Enter quantity..." />
+            <label htmlFor="price">Price</label>
+            <input type="number" id="price" name="price" placeholder="Enter price..." />
+            <button type="submit">Buy</button>
+            <button type="submit">Sell</button>
+        </form>
     </div>
   );
 }
