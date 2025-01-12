17032cc75a7f90c2b3d2b9d3c8afadee731395f6
vepaan <141347566+vepaan@users.noreply.github.com>
2025-01-11 22:34:58 -0600

    frontend update

--- frontend/src/pages/placeorder.js
+++ frontend/src/pages/placeorder.js
@@ -16,6 +16,8 @@ function PlaceOrder() {
       price,
       orderType,
       duration,
+      stoploss,
+      leverage,
       action,
     };
     console.log(orderDetails);  // Here you can handle the form submission
@@ -24,77 +26,104 @@ function PlaceOrder() {
   return (
     <div className="order-form">
       <form className="order-form-space">
+        {/* Quantity and Price */}
         <div className="form-row">
-          <input
-            type="number"
-            className="qty-box"
-            id="quantity"
-            name="quantity"
-            placeholder="Enter quantity..."
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-          />
-          <input
-            type="number"
-            className="price-box"
-            id="price"
-            name="price"
-            placeholder="Enter price..."
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
+          <div className="label">Qty</div>
+          <div className="label">Price</div>
+        </div>
+        <div className="form-row">
+          <div className="input-field">
+            <input
+              type="number"
+              className="qty-box"
+              id="quantity"
+              name="quantity"
+              placeholder="Enter quantity..."
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+            <span className="currency-sign">$</span>
+          </div>
+          <div className="input-field">
+            <input
+              type="number"
+              className="price-box"
+              id="price"
+              name="price"
+              placeholder="Enter price..."
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <span className="currency-sign">$</span>
+          </div>
         </div>
 
+        {/* Order Type and Duration */}
         <div className="form-row">
-          <label htmlFor="orderType" className="order-type-label">
-            Order Type
-          </label>
-          <select
-            id="orderType"
-            name="orderType"
-            className="order-type-dropdown-1"
-            value={orderType}
-            onChange={(e) => setOrderType(e.target.value)}
-          >
-            <option value="market">Market (MKT)</option>
-            <option value="limit">Limit (LMT)</option>
-          </select>
-          <select
-            id="duration"
-            name="duration"
-            className="order-type-dropdown-2"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-          >
-            <option value="day">Day</option>
-            <option value="GTC">Good Til Canceled</option>
-            <option value="IOC">Immediate Or Cancel</option>
-            <option value="FOK">Fill Or Kill</option>
-          </select>
+          <div className="label">Order Type</div>
+          <div className="label">Time in Force</div>
+        </div>
+        <div className="form-row">
+          <div className="input-field">
+            <select
+              id="orderType"
+              name="orderType"
+              className="order-type-dropdown-1"
+              value={orderType}
+              onChange={(e) => setOrderType(e.target.value)}
+            >
+              <option value="market">Market (MKT)</option>
+              <option value="limit">Limit (LMT)</option>
+            </select>
+          </div>
+
+          <div className="input-field">
+            <select
+              id="duration"
+              name="duration"
+              className="order-type-dropdown-2"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+            >
+              <option value="day">Day</option>
+              <option value="GTC">Good Til Canceled</option>
+              <option value="IOC">Immediate Or Cancel</option>
+              <option value="FOK">Fill Or Kill</option>
+            </select>
+          </div>
         </div>
 
+        {/* Stop Loss and Leverage */}
         <div className="form-row">
-          Stop Loss
-        <input
-            type="number"
-            className="stop-loss"
-            id="stoploss"
-            name="stoploss"
-            value={stoploss}
-            onChange={(e) => setStopLoss(e.target.value)}
-          />
-          Leverage
-          <input
-            type="number"
-            className="leverage"
-            id="leverage"
-            name="leverage"
-            value={leverage}
-            onChange={(e) => setLevergae(e.target.value)}
-          />
-          x
+          <div className="label">Stop Loss</div>
+          <div className="label">Leverage</div>
+        </div>
+        <div className="form-row">
+          <div className="input-field">
+            <input
+              type="number"
+              className="stop-loss"
+              id="stoploss"
+              name="stoploss"
+              value={stoploss}
+              onChange={(e) => setStopLoss(e.target.value)}
+            />
+            <span className="currency-sign">$</span>
+          </div>
+          <div className="input-field">
+            <input
+              type="number"
+              className="leverage"
+              id="leverage"
+              name="leverage"
+              value={leverage}
+              onChange={(e) => setLevergae(e.target.value)}
+            />
+            <span className="currency-sign">x</span>
+          </div>
         </div>
 
+        {/* Action Buttons */}
         <div className="form-row">
           <button
             type="submit"
