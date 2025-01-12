e1ed9b2399ae7ac452c1e28fc178816a007638fa
vepaan <141347566+vepaan@users.noreply.github.com>
2025-01-11 19:46:40 -0600

    added order settings

--- frontend/src/pages/placeorder.js
+++ frontend/src/pages/placeorder.js
@@ -1,17 +1,28 @@
 import React, {useEffect, useState, useRef} from "react";
-import '../App.css';
+import '../placeorder.css';
 
 function PlaceOrder() {
   return (
     <div className="order-form">
         <form>
-            <label htmlFor="quantity">Quantity</label>
+          <div>
             <input type="number" id="quantity" name="quantity" placeholder="Enter quantity..." />
-            <label htmlFor="price">Price</label>
             <input type="number" id="price" name="price" placeholder="Enter price..." />
+          </div>  
+          <div>
             <button type="submit">Buy</button>
             <button type="submit">Sell</button>
-            <button type="submit">Short Sell</button> 
+            <button type="submit">Short Sell</button>
+          </div>
+          <div>
+            <label for="orderType">Order Type:</label>
+            <select id="orderType" name="orderType">
+              <option value="market">Market</option>
+              <option value="limit">Limit</option>
+              <option value="stop">Stop</option>
+              <option value="stop-limit">Stop Limit</option>
+            </select>
+          </div>
         </form>
     </div>
   );
