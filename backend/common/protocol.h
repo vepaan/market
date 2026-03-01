9bdd62cdc30939d4a52f5cc98aa354e5fee43b95
vepaan <141347566+vepaan@users.noreply.github.com>
2026-02-28 19:53:33 -0600

    some progress on protcol for exchange

---
+++ backend/common/protocol.h
@@ -0,0 +1,71 @@
+#ifndef EXCHANGE_PROTOCOL_H
+#define EXCHANGE_PROTOCOL_H
+
+#include <cstdint>
+
+namespace Exchange
+{
+    #pragma pack(push, 1)
+
+    struct MarketUpdate 
+    {
+        uint32_t tickerId;
+        double price;
+        uint32_t volume;
+        uint64_t timestamp;
+        char side; // 'B'=Bid, 'A'=Ask, 'T'=Trade
+    };
+
+    #pragma pack(pop)
+
+    enum class OrderType : uint8_t
+    {
+        Limit = 0,
+        Market = 1,
+        Iceberg = 2
+    };
+
+    enum class TimeInForce : uint8_t
+    {
+        GTC = 0, // Good Till Cancelled
+        IOC = 1, // Immediate Or Cancel
+        FOK = 2 // Fill Or Kill
+    };
+
+    #pragma pack(push, 1)
+
+    struct OrderRequest 
+    {
+        uint32_t clientId;
+        uint32_t clientOrderId;
+        uint32_t tickerId;
+
+        char side; // 'B'=Bid, 'A'=Ask
+        OrderType type;
+        TimeInForce tif;
+        uint8_t padding;
+
+        double price;
+        uint32_t volume;
+        uint32_t visibleVolume;
+
+        uint64_t timestamp;
+    };
+
+    #pragma pack(pop)
+
+    class Gateway
+    {
+    public:
+
+        Gateway();
+
+        ~Gateway();
+
+    private:
+        
+
+    };
+}
+
+#endif
